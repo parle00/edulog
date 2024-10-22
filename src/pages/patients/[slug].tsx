1611f1123a6bae7b83f0b994fe0671adf68e4dbc
Yunus Emre <yunus1481emre@gmail.com>
2024-10-22 10:17:40 +0300

    update

--- src/pages/patients/[slug].tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from "next/router";
-import React from "react";
-
-const DetailPage = () => {
-  const router = useRouter();
-  return <div>{router.query.slug}</div>;
-};
-
-export default DetailPage;
